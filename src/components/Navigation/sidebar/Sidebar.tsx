1c0e09670cdcaef105d39c1c90702a5b6def692d
Habsyi2512 <githabsyi@gmail.com>
2024-11-02 18:08:56 +0700

    "Update package.json dependencies and modify Sidebar and NavbarContext components"

--- src/components/Navigation/sidebar/Sidebar.tsx
+++ src/components/Navigation/sidebar/Sidebar.tsx
@@ -29,18 +29,30 @@ export default function Sidebar() {
     <aside
       onClick={handleClickOutside}
       className={`fixed w-full lg:w-fit h-screen lg:h-auto top-0 left-0 lg:relative tracking-wide ${
-        isOpenSidebar ? "lg:block" : "hidden lg:block"
+        isOpenSidebar
+          ? "transition-transform duration-200"
+          : "-translate-x-full lg:translate-x-0 duration-0"
       } bg-white/30 backdrop-blur-sm`}
     >
-      <div className="w-72 max-w-[90%] p-8 lg:p-0 bg-white lg:bg-auto border-r h-full lg:h-fit lg:border-r-0 sticky top-[119px]">
-        <button
-          onClick={() => {
-            setIsOpenSidebar(false);
-          }}
-          className="absolute group top-8 right-8 lg:hidden"
-        >
-          <XMarkIcon className="size-6 text-slate-400 group-hover:text-slate-500 group-active:text-sate-400" />
-        </button>
+      <div className="w-72 max-w-[90%] p-8 lg:p-0 bg-white lg:bg-auto border-r rounded-r-lg h-full lg:h-fit lg:border-r-0 sticky top-[119px]">
+        <div className="lg:hidden flex items-center justify-between mb-5">
+          <div className="h-full flex items-center w-fit">
+            <h1 className="text-2xl text-colorSky">
+              <span className="font-bold">C</span>
+              <span className="text-base">in</span>
+              <span className="font-bold">C</span>
+              <span className="ordinal ">official</span>
+            </h1>
+          </div>
+          <button
+            onClick={() => {
+              setIsOpenSidebar(false);
+            }}
+            className="group"
+          >
+            <XMarkIcon className="size-6 text-slate-400 group-hover:text-slate-500 group-active:text-sate-400" />
+          </button>
+        </div>
 
         <SidebarGroup>
           <SidebarGroupLabel>About</SidebarGroupLabel>
--- src/context/NavbarContext.tsx
+++ src/context/NavbarContext.tsx
@@ -24,14 +24,50 @@ export default function NavbarContextProvider({
   children: ReactNode;
 }) {
   const [isOpenSidebar, setIsOpenSidebar] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
   useEffect(() => {
     if (isOpenSidebar) {
       document.body.classList.add("overflow-hidden");
     } else {
       document.body.classList.remove("overflow-hidden");
     }
-    // Bersihkan efek saat komponen tidak aktif
     return () => document.body.classList.remove("overflow-hidden");
+  }, [isOpenSidebar]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        setWindowWidth(window.innerWidth);
+      }, 100);
+    };
+
+    let timeoutId: NodeJS.Timeout;
+    window.addEventListener("resize", handleResize);
+    return () => {
+      clearTimeout(timeoutId);
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  useEffect(() => {
+    let previousWidth = window.innerWidth;
+
+    const handleResize = () => {
+      const currentWidth = window.innerWidth;
+
+      if (previousWidth >= 1024 && currentWidth < 1024 && isOpenSidebar) {
+        setIsOpenSidebar(false);
+      }
+
+      // Update previousWidth untuk digunakan pada event resize berikutnya
+      previousWidth = currentWidth;
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Bersihkan event listener saat komponen dilepas
+    return () => window.removeEventListener("resize", handleResize);
   }, [isOpenSidebar]);
 
   return (
