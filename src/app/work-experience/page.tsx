9893f7e146093d3f91233ac30ace0e52d6cd5211
e-ikona <Vernanda089@gmail.com>
2024-11-03 21:57:47 +0700

    lo

--- src/app/work-experience/page.tsx
+++ src/app/work-experience/page.tsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import { Paragraph, TextTitle } from "@/components/typography/Typography";
+
 
 export default function Page() {
   return (
-    <div>Halaman Work experience</div>
+    <div>
+      <TextTitle title="Work Experience" />
+      <Paragraph>
+      Kami mempunyai pengalaman mengerjakan proyek dengan berbagai bahasa seperti berikut.
+      </Paragraph>
+      <br />
+       <ul className="list-disc ml-6 space-y-1">
+        <li><strong>NEXT JS</strong> 
+        <br />
+        Membantu perusahaan merancang roadmap teknologi yang selaras dengan visi jangka panjang.</li>
+        <br />
+        <li><strong>LARAVEL</strong> 
+        <br />
+        Membantu perusahaan merancang roadmap teknologi yang selaras dengan visi jangka panjang.</li>
+        <br />
+        <li><strong>FLASK</strong> 
+        <br />
+        Membantu perusahaan merancang roadmap teknologi yang selaras dengan visi jangka panjang.</li>
+        <br />
+        <li><strong>EXPRESS  JS</strong> 
+        <br />
+        Membantu perusahaan merancang roadmap teknologi yang selaras dengan visi jangka panjang.</li>
+        <br />
+      </ul>
+    </div>
   )
 }
